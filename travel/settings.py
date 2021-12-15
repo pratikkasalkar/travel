bd54a66323f285a58bfe734d0ab252252122ce18
pratikkasalkar <pratikkasalkar@gmail.com>
2021-12-15 19:48:02 +0000

    changed db

--- travel/settings.py
+++ travel/settings.py
@@ -72,13 +72,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'travel.wsgi.application'
 
 
-# Database
+# python manage.py migrate
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'nci',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': 'nci-common.c8jevb6pjd8h.eu-west-1.rds.amazonaws.com',
+
+        'PORT': '5432',
     }
 }
 
